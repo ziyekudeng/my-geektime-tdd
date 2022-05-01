4ee150f22bf8fb065970421ef1a3615b57aa18ef
pengjunzhen <pengjunzhenwork@163.com>
2022-05-02 00:17:19 +0800

    feat: 构思实现功能的方式

--- src/test/java/geektime/tdd/args/ArgsTest.java
+++ src/test/java/geektime/tdd/args/ArgsTest.java
@@ -3,7 +3,22 @@ package geektime.tdd.args;
 import org.junit.jupiter.api.Test;
 
 public class ArgsTest {
+    // 策略一：正则切割，比较复杂
     // -l -p 8080 -d /usr/logs
+
+    // 策略二：拆分成数组
+    // 实际上我们可以把它看成是一系列以dash开头的标志，然后把这个数组进行分割
+    // 分割成的数组如下。分完段之后，我们就可以由对应的Annotation 标注好的类型结构里面去读
+    // 读取的时候时候再根据它生成的新的小结构转换成对应的这样一个类型。
+    // [-l], [-p, 8080], [-d, /usr/logs]
+    // 上面这种是我们选择的实现策略
+
+    // ①像上面那种利用数组进行分段，我们可以利用一个index 标志位，找到每一个位置，然后去读。
+    // ②我们也可以把这么个list做成一个特定的interface对它进行整体的分类处理
+    // 这里我们选那种写法都可以，还有map的写法等
+
+    // 策略三：map
+    // {-l:[], -p:[8080], -d:[/usr/logs] }
 
     @Test
     public void should() {
